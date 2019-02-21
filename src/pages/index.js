890055c85b632dfb8c69c0a559b594d9f92066ab
JordiiV <jordii94@gmail.com>
2019-02-21 16:44:11 +0100

    changes in index location folder

---
+++ src/pages/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Link} from 'gatsby'
+import Typing from 'react-typing-animation';
+import Layout from '../components/Layout'
+import BlogRoll from '../components/BlogRoll'
+import '../components/main.scss'
+
+
+export const IndexPageTemplate = () => (
+    <div>
+        <section className={"hero is-fullheight hero-foo is-medium is-hidden-mobile is-dark"}>
+          <div className="hero-body" style={{ margin: '5% 0px 0px 0px' }}>
+            <div className="container container-mobile" style={{ marginTop: "-8%", padding: "11% 13% 10% 5%" }}>
+              <Typing>
+                <h1 className="title" style={{ color: 'white', fontWeight: "700", fontSize: "4rem" }}>
+                  Hi
+              </h1>
+                <Typing.Speed ms={150} />
+                <h1 className="title" style={{ color: 'white', fontWeight: "700", fontSize: "4rem" }}>
+                  I'm the Mead Baron
+              </h1>
+                <Typing.Speed ms={10} />
+                <h2 className="title" style={{ color: 'red' }}>(This site is under construction!)</h2>
+              </Typing>
+            </div>
+          </div>
+        </section>
+        <div className="section">
+          <div className="column is-12">
+            <h3 className="has-text-weight-semibold is-size-2">
+              Latest stories
+                  </h3>
+            <BlogRoll />
+            <div className="column is-12 has-text-centered">
+              <Link className="btn" to="/blog">
+                Read more
+              </Link>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+
+IndexPageTemplate.propTypes = {
+}
+
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <IndexPageTemplate
+      />
+    </Layout>
+  )
+}
+
+export default IndexPage
+
