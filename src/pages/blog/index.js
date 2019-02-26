e25f85cfa325b95c637efcc81d8ebf592a6504a6
JordiiV <jordii94@gmail.com>
2019-02-26 03:22:31 +0100

    changed blog header

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -7,7 +7,7 @@ export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <section>
+          <section className="section">
           <div className="container">
             <div className="content">
               <div
